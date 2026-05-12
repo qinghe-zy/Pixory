dc5cc07f4e65e3bb3d40c633c737eef9e279004c
qinghe <3339868271@qq.com>
2026-05-12 17:44:21 +0800

    chore: release pixory 2.0.8

--- src/services/updateCheckService.ts
+++ src/services/updateCheckService.ts
@@ -1,8 +1,8 @@
 import Constants from 'expo-constants';
 
 const DEFAULT_TIMEOUT_MS = 5000;
-const FALLBACK_CURRENT_VERSION = '2.0.7';
-const FALLBACK_CURRENT_VERSION_CODE = 207;
+const FALLBACK_CURRENT_VERSION = '2.0.8';
+const FALLBACK_CURRENT_VERSION_CODE = 208;
 
 export interface AppUpdateInfo {
   version: string;
